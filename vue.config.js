f77390b4aef790caa64beda4d7d05b319bba2cc8
zqm02 <101574538+zqm02@users.noreply.github.com>
2023-07-10 11:26:07 +0800

    commit

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+"use strict";
+const path = require("path");
+const resolve = (dir) => path.join(__dirname, dir);
+
+module.exports = {
+  chainWebpack: (config) => {
+    config.resolve.alias.set("@", resolve("src"));
+  },
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://test.my-site.com",
+      },
+    },
+  },
+};
